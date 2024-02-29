e7e812a5b335d2a42c335fec2472dce6baa0150c
momoladebrouill <momoladebrouill@gmail.com>
2024-02-29 21:37:36 +0100

    Je vais etouffer le mec qui a inventé fetch 😇️

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+
+let b = [];
+
+var req = fetch('./data2.json')
+    .then((response) => response.json())
+    .then((data) => data)
+// get data from request
+let data = req;
+console.log(data);
+
+// toma b part
 document.getElementById('button').addEventListener('click', function() {
     let value = document.getElementById('input').value;
     let translated = ckoalafonction(value)
@@ -7,11 +18,20 @@ document.getElementById('button').addEventListener('click', function() {
 //generé pas copilot
 function ckoalafonction(value) {
     let translated = '';
+    console.log(value);
+    let data = req;
     for (let i = 0; i < value.length; i++) {
-        if (value[i] === 'a' || value[i] === 'A') {
-            translated += 'akoala';
-        } else {
-            translated += value[i];
+        for(elem in data){
+            console.log("elem");
+            console.log(elem);
+            done = false;
+            if (value[i] === elem['nom'][0]|| value[i] === 'A') {
+                translated += elem['nom'];
+                done = true;
+            }
+            if(done == false){
+                translated += value[i];
+            }
         }
     }
     return translated;
